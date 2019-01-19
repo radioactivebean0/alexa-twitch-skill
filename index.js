979c9551dbb6da1de957017462a99aa325ede3c9
Scott Simenel <scott.simenel@gmail.com>
2019-01-19 13:58:28 -0800

    Added more get requests

--- index.js
+++ index.js
@@ -2,16 +2,35 @@ var axios = require('axios');
 function getTopStreams(num = 5) {
     const topStreamsURL = 'https://api.twitch.tv/helix/streams?first=' + num;
     axios.get(topStreamsURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
-        .then(data => console.log(data.data))
+        .then(res => console.log(res.data))
         .catch(err => console.log(err));
 }
+
+function getTopGameStreams(gameID, num = 5) {
+    const topGameStreamsURL = 'https://api.twitch.tv/helix/streams?first=' + num + '&game_id=' + gameID;
+    axios.get(topGameStreamsURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
+}
+
 
 function getTopGames(num = 5) {
     const topGamesURL = 'https://api.twitch.tv/helix/games/top?first=' + num;
     axios.get(topGamesURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
-        .then(data => console.log(data.data))
+        .then(res => console.log(res.data))
         .catch(err => console.log(err));
 }
 
+function getStreamInfo(userLogin) {
+    const streamURL = 'https://api.twitch.tv/helix/streams?user_login=' + userLogin;
+    axios.get(streamURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err));
+}
+
+getTopGameStreams(33214);
+/*
+getStreamInfo('ninja');
 getTopStreams(3);
 getTopGames(1);
+*/
