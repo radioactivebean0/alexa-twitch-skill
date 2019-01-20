cb52e1c8ea1105f67c7721d20aed75d9a696682f
Scott Simenel <scott.simenel@gmail.com>
2019-01-19 16:21:37 -0800

    Swapped out existing get request with Axios versions

--- serverlessrepo-twitchchat-alexaskillskitnodejsfact-1RL4LUFLMQY4O-814c9d04-281b-489c-958f-fc1fc6bf6bba/index.js
+++ serverlessrepo-twitchchat-alexaskillskitnodejsfact-1RL4LUFLMQY4O-814c9d04-281b-489c-958f-fc1fc6bf6bba/index.js
@@ -115,62 +115,48 @@ exports.handler = skillBuilder
   .addErrorHandlers(ErrorHandler)
   .lambda();
 
-var data;
-function getData(name) {
-  if (name == null) {
-
-  }
-
-  var request = require('request');
-
-  var headers = {
-    'Client-ID': '76cpa8o345vq4w52p2s4utyzoxsfd5'
-  };
-
-  var options = {
-    url: 'https://api.twitch.tv/helix/streams?user_login=' + name,
-    headers: headers
-  };
-
-  data = {};
-
-  function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var tempData = JSON.stringify(JSON.parse(body).data);
-      data = JSON.parse(tempData.substring(1, tempData.length - 1));
-    }
-  }
-
-  request(options, callback);
-
-  function getTopGameStreams(gameName, num = 5) {
-    const gameURL = 'https://api.twitch.tv/helix/games?name=' + gameName;
-    return new Promise(function (resolve, reject) {
-      axios.get(gameURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
-        .then(res => {
-          const gameID = res.data.data[0].id;
-          const topGameStreamsURL = 'https://api.twitch.tv/helix/streams?first=' + num + '&game_id=' + gameID;
-          axios.get(topGameStreamsURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
-            .then(res => resolve(res.data))
-            .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err));
-    });
-  }
-
-  function getTopGames(num = 5) {
-    const topGamesURL = 'https://api.twitch.tv/helix/games/top?first=' + num;
-    return new Promise(function (resolve, reject) {
-      axios.get(topGamesURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
-        .then(res => resolve(res.data))
-        .catch(err => console.log(err));
-    });
-  }
-  function getStreamName() {
-
-    return data.title;
-  }
-  function getViewers() {
-    return data.viewer_count;
-  }
+function getTopStreams(num = 5) {
+  const topStreamsURL = 'https://api.twitch.tv/helix/streams?first=' + num;
+  return new Promise(function (resolve, reject) {
+    axios.get(topStreamsURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+      .then(res => resolve(res.data))
+      .catch(err => console.log(err));
+  });
 }
+
+function getTopGameStreams(gameName, num = 5) {
+  const gameURL = 'https://api.twitch.tv/helix/games?name=' + gameName;
+  return new Promise(function (resolve, reject) {
+    axios.get(gameURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+      .then(res => {
+        const gameID = res.data.data[0].id;
+        const topGameStreamsURL = 'https://api.twitch.tv/helix/streams?first=' + num + '&game_id=' + gameID;
+        axios.get(topGameStreamsURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+          .then(res => resolve(res.data))
+          .catch(err => console.log(err));
+      })
+      .catch(err => console.log(err));
+  });
+}
+
+function getTopGames(num = 5) {
+  const topGamesURL = 'https://api.twitch.tv/helix/games/top?first=' + num;
+  return new Promise(function (resolve, reject) {
+    axios.get(topGamesURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+      .then(res => resolve(res.data))
+      .catch(err => console.log(err));
+  });
+}
+
+
+function getStreamInfo(userLogin) {
+  const streamURL = 'https://api.twitch.tv/helix/streams?user_login=' + userLogin;
+  return new Promise(function (resolve, reject) {
+    axios.get(streamURL, { headers: { 'Client-ID': 'n6tkzhwjlttoho9gnpid51z840eiv3' } })
+      .then(res => {
+        resolve(res.data);
+      })
+      .catch(err => console.log(err));
+  });
+}
+
